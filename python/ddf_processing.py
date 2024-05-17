7fe7a204ddaca330a30598d1abbc2a05a831313f
parinbhaduri <pbb62@cornell.edu>
2024-05-17 08:47:48 -0400

    Moved processing files; Created BG aggregated data
    Moved processing files to a separate folder. Created csv file that holds damage estimates at the block group level

---
+++ python/ddf_processing.py
@@ -0,0 +1,16 @@
+###Aggregate ddf damage files to block group scale###
+from os.path import join
+from pathlib import Path
+import pandas as pd
+
+FO = Path().absolute()
+fd_bg = pd.read_parquet(join(FO, 'model_inputs','ddfs','base_df.pqt')).set_index('fd_id')
+ens_agg_df = pd.read_parquet(join(FO, 'model_inputs','ddfs','ensemble_agg.pqt'))
+
+#join two dataframe on fd_id
+ens_agg_df = ens_agg_df.join(fd_bg[['bg_id']])
+#Group on block group. Combine through summation
+ens_agg_df.groupby('bg_id').sum()
+
+#Save to csv file
+ens_agg_df.to_csv(join(FO, 'model_inputs','ddfs','ens_agg_bg.csv'))
