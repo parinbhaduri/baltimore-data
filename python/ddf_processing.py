5f25901cdac633a768b6c911ec8abbc9f7668051
parinbhaduri <pbb62@cornell.edu>
2024-05-30 11:51:46 -0400

    Aggregated ddfs to average by BG
    Edited BG ddf csv file to provide average loss values per BG, instead of cumulative loss

--- python/ddf_processing.py
+++ python/ddf_processing.py
@@ -9,8 +9,8 @@ ens_agg_df = pd.read_parquet(join(FO, 'model_inputs','ddfs','ensemble_agg.pqt'))
 
 #join two dataframe on fd_id
 ens_agg_df = ens_agg_df.join(fd_bg[['bg_id']])
-#Group on block group. Combine through summation
-ens_agg_df = ens_agg_df.groupby('bg_id').sum()
+#Group on block group. Combine through expectation to get average loss per block group
+ens_agg_df = ens_agg_df.groupby('bg_id').mean()
 
 #Save to csv file
 ens_agg_df.to_csv(join(FO, 'model_inputs','ddfs','ens_agg_bg.csv'))
